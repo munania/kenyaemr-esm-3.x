9b724f5b2ae9f5ae866adf1f7f3208d76375844d
munania <munaniadeno@gmail.com>
2024-07-03 19:16:20 +0300

    FIXED RECOMMENDED CHANGES

--- packages/esm-billing-app/src/past-patient-bills/patient-bills-dashboard/patient-bills-dashboard.tsx
+++ packages/esm-billing-app/src/past-patient-bills/patient-bills-dashboard/patient-bills-dashboard.tsx
@@ -3,12 +3,6 @@ import styles from './patient-bills-dashboard.scss';
 import { ExtensionSlot } from '@openmrs/esm-framework';
 import PastPatientBills from '../filtered-patient-bills.component';
 import { useBills } from '../../billing.resource';
-import { Dropdown } from '@carbon/react';
-
-const filterItems = [
-  { id: 'PENDING', text: 'Pending bills' },
-  { id: 'POSTED', text: 'Posted bills' },
-];
 
 const PatientBillsScreen: React.FC = () => {
   const [patientUuid, setPatientUuid] = React.useState<string>('');
